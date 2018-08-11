5ff1fd6bd4cf13020943b8357fe93b616b9cfcd4
DERRICK LORD <derricklord@derrick-pro.local>
2018-08-11 11:56:41 -0700

    var to const in index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var http = require('http');
+const http = require('http');
+
+
 
 var server = http.createServer(function(request, response) {
 
@@ -7,7 +9,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = process.env.PORT || 1337;
+const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
